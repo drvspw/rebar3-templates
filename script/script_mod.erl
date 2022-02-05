79f62b8780bf66022ca84eadb3b5616165f9e6cd
Venkatakumar Srinivasan <venkat@drvs.pw>
2022-02-05 15:13:28 +0530

    updates

--- script/script_mod.erl
+++ script/script_mod.erl
@@ -13,57 +13,68 @@
 %% API functions
 %%====================================================================
 main(Args) ->
-  {ok, _} = application:ensure_all_started(?APP_NAME),
-  OptSpecList = option_spec_list(),
-  Cmd = getopt:parse(OptSpecList, Args),
-  exec(Cmd).
+    {ok, _} = application:ensure_all_started(?APP_NAME),
+    OptSpecList = option_spec_list(),
+    Cmd = getopt:parse(OptSpecList, Args),
+    exec(Cmd).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 exec({ok, {[], []}}) ->
-  help();
+    help();
 
 exec({ok, {_Options, [?CMD_VERSION]}}) ->
-  version();
+    version();
 
 exec({ok, {_Options, [?CMD_HELP]}}) ->
-  help();
+    help();
 
 exec({ok, {Options, [Command]}}) ->
-  try
-    execute_command(Command, maps:from_list(Options))
-  catch
-    _ : Reason ->
-      io:format("ERROR: ~p~n", [Reason]),
-      erlang:halt(1)
-  end;
+    try
+        execute_command(Command, maps:from_list(Options))
+    catch
+        _ : Reason ->
+            io:format("ERROR: ~p~n", [Reason]),
+            erlang:halt(1)
+    end;
 
 exec({ok, {_, _}}) ->
-  help();
+    help();
 
 exec({error, {Reason, Data}}) ->
-  io:format("~s ~p~n", [Reason, Data]),
-  help(),
-  erlang:halt(1).
+    io:format("~s ~p~n", [Reason, Data]),
+    help(),
+    erlang:halt(1).
 
 execute_command(_, _) ->
-  help().
+    help().
+
+%% options spec list
+%% {Key, $Character, "StringName", Spec, HelpText}
+%% {port, $p, "port", {integer, 5432}, "Database server port"}
+option_spec_map() ->
+    #{
+      %% cmd => Options
+     }.
 
 option_spec_list() ->
-  [
-   %% options spec list
-   %% {Key, $Character, "StringName", Spec, HelpText}
-   %% {port, $p, "port", {integer, 5432}, "Database server port"}
-  ].
+    Map = option_spec_map(),
+    FoldFn = fun(_, Options, Acc) ->
+                     Acc ++ Options
+             end,
+    maps:fold(FoldFn, [], Map).
 
 help() ->
-  %% Print version
-  version(),
+    %% Print version
+    version(),
 
-  %% Print usage
-  OptSpecList = option_spec_list(),
-  getopt:usage(OptSpecList, atom_to_list(?APP_NAME), "COMMANDS", []).
+    %% Print usage
+    AppName = atom_to_list(?APP_NAME),
+    OptSpecMap = option_spec_map(),
+    maps:foreach( fun(Cmd, Options) ->
+                          getopt:usage(Options, AppName, Cmd, [])
+                  end, OptSpecMap).
 
 version() ->
     case lists:keyfind(?APP_NAME, 1, application:loaded_applications()) of
